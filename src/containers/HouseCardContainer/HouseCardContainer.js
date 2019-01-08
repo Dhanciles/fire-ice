021a9aded60ff91373a5b1c76d6f6af0b3aa4a39
Derek Hanciles <drhanciles@icloud.com>
2019-01-08 10:39:34 -0700

    HouseCardContainer component is now conneceted to the store

---
+++ src/containers/HouseCardContainer/HouseCardContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchData } from '../../thunks/fetchData.js'
+
+export class HouseCardContainer extends Component {
+  constructor() {
+    super()
+  }
+  componentDidMount() {
+    const url = 'http://localhost:3001/api/v1/houses'
+    this.props.fetchData(url)
+  }
+
+  render() {
+    const housesToRender = this.state.houseData.map(house => (<HouseCard {...house}/>))
+    return (
+      <main>
+        { housesToRender }
+      </main>
+    )
+  }
+}
+
+export const mapStateToProps = (state) => ({
+  houseData: state.houseData, 
+  error: state.hasErrored, 
+  isLoading: state.isLoading
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  fetchData: (url) => dispatch(fetchData(url))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HouseCardContainer)
