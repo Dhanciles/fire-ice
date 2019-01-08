6cc23d1c44770ae7294ad8ee5f32a893baeb9dc7
Derek Hanciles <drhanciles@icloud.com>
2019-01-08 09:57:15 -0700

    Implemented thunk folder which now contains a thunk that will fetch the data we want

---
+++ src/thunks/fetchData.js
@@ -0,0 +1,20 @@
+import { isLoading, hasErrored, fetchDataSuccess } from '../actions.index.js'
+import { houseCleaner } from '../cleaner/cleaner.js'
+
+export const fetchData = (url) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url)
+      if(!response.ok) {
+        throw Error (response.statusText)
+      }
+      dispatch(isLoading(false))
+      const data = await response.json
+      const cleanData = houseCleaner(data)
+      dispatch(fetchDataSuccess(cleanData))
+    } catch(error) {
+      dispatch(hasErrored(true))
+    }
+  }
+}
