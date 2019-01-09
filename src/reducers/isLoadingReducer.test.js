73cf0c9b7157c9c5a7e9c11e0a079f57662c667c
Derek Hanciles <drhanciles@icloud.com>
2019-01-09 10:44:59 -0700

    Test checking if isLoadingReducer is updating state has been impelemented and is passing

--- src/reducers/isLoadingReducer.test.js
+++ src/reducers/isLoadingReducer.test.js
@@ -7,6 +7,18 @@ describe('isLoadingReducer', () => {
     const result = isLoadingReducer(undefined, {})
 
     expect(result).toEqual(expected)
-
   })
+  it('should update state if isLoading action is true', () => {
+    const action = {
+      type: 'IS_LOADING', 
+      isLoading: true
+    }
+
+    const expected = true
+  
+    const result = isLoadingReducer(false, action)
+  
+    expect(result).toEqual(expected)
+  })
+  
 })
