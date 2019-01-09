f2241f43957f820a2021e869eaa95e3f11320c93
Derek Hanciles <drhanciles@icloud.com>
2019-01-09 10:49:21 -0700

    test checking if hasErroredReducer is updating state has been implemented and is passing

--- src/reducers/hasErroredReducer.test.js
+++ src/reducers/hasErroredReducer.test.js
@@ -8,4 +8,16 @@ describe('hasErroredReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should update state if hasErrored action is true', () => {
+    const action = {
+      type: 'HAS_ERRORED', 
+      hasErrored: true
+    }
+
+    const expected = true
+  
+    const result = hasErroredReducer(false, action)
+  
+    expect(result).toEqual(expected)
+  })
 })
